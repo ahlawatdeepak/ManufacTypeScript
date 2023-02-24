80fb0e2baebc8dc73a40327f33975b8c9cfefa9f
Deepak <103956638+ahlawatdeepak@users.noreply.github.com>
2023-02-24 11:16:27 +0530

    Update ScatterChart.tsx

--- src/Components/ScatterChart.tsx
+++ src/Components/ScatterChart.tsx
@@ -12,6 +12,11 @@ const color= AllData.map((el) => {
   });
   
   export const ScatterChart = () => {
+      
+      
+       // initializes an option object to configure the settings for a Scatter chart.
+
+      //  The Color Data and Hue Data arrays are used to populate the x-axis and y-axis data in the chart.
     const option = {
       title: {
         text: 'Color Intensity and Hue Data',
